03b93d91bce41ff6321c508ad13be18e32a3ae46
Mochi-frontend <hayabusa06mochi@gmail.com>
2024-06-10 11:28:04 +0900

    fixed

--- src/main/java/com/example/common/GlobalExceptionHandler.java
+++ src/main/java/com/example/common/GlobalExceptionHandler.java
@@ -1,23 +1,34 @@
 package com.example.common;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
-@ControllerAdvice
-public class GlobalExceptionHandler {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-    @ExceptionHandler(NoHandlerFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleNotFound() {
-        return "error/404";
-    }
+/**
+ * アプリケーション内で処理されなかった例外をここでキャッチし、
+ * エラーページへ遷移させます.
+ *
+ * @autor hayato.mochinaga
+ */
+@Component
+public class GlobalExceptionHandler implements HandlerExceptionResolver {
 
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String handleInternalServerError() {
-        return "error/500";
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @Override
+    public ModelAndView resolveException(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object obj,
+            Exception e) {
+        LOGGER.error("500 Error: Internal server error", e);
+        return null;
     }
 }
